df394dea9d5c57c1bb9a1ca497e6a9ca7f7cd398
mwu <mwu@mobi.byss>
2022-06-18 20:36:07 +0200

    fixed? camera middle point

--- Assets/_Game/Scripts/CameraMiddlePointTracker.cs
+++ Assets/_Game/Scripts/CameraMiddlePointTracker.cs
@@ -6,15 +6,21 @@ public class CameraMiddlePointTracker : MonoBehaviour
 {
     public List<PlayerMovement> _listOfPlayers = new List<PlayerMovement>();
 
+
     private void Update()
     {
         FindPlayers();
 
         if (_listOfPlayers.Count > 0)
         {
-            Vector3 player1Pos = _listOfPlayers[0].transform.position;
-            Vector3 player2Pos = _listOfPlayers.Count>=2 ?  _listOfPlayers[1].transform.position : _listOfPlayers[0].transform.position;
-            transform.position = Vector3.Lerp(player1Pos, player2Pos, 0.5f);
+            if (_listOfPlayers.Count == 1)
+            {
+                transform.position = _listOfPlayers[0].transform.position;
+            }
+            else if (_listOfPlayers.Count == 2)
+            {
+                transform.position = Vector3.Lerp(_listOfPlayers[0].transform.position, _listOfPlayers[1].transform.position, 0.5f);
+            }
         }
     }
 
