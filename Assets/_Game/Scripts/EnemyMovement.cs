e5e02563e33b5ec8e2043ecb7dcea24f456474b5
DamianWk <dwaleciuk@ignition-interactive.com>
2022-06-18 21:39:18 +0200

    enemy skok i move

--- Assets/_Game/Scripts/EnemyMovement.cs
+++ Assets/_Game/Scripts/EnemyMovement.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    public float enemySpeed = 2;
+    public float enemySpeed = 2f;
+    public bool playerClose = false;
+    public float jumpForce = 2f;
+    public Collider2D moveTriger;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
-        _rb.velocity = new Vector2(-enemySpeed, _rb.velocity.y);
+        if (playerClose == true) {
+            _rb.velocity = new Vector2(-enemySpeed, _rb.velocity.y);
+        }
+       
     }
+
+    private void OnTriggerEnter2D(Collider2D collider2D) {
+        if(collider2D.gameObject.layer == 6 || collider2D.gameObject.layer == 7) {
+            playerClose = true;
+            moveTriger.enabled = false;
+        }
+    }
+
+    public void Jump() {
+        _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+    }
+
 }
